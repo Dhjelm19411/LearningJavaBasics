e21a1647c652fc5302e2a6a44624fa58b0cde97e
David Hjelm <D2hjelm@gmail.com>
2018-09-10 00:28:45 -0400

    Initial commit

---
+++ src/learningjavabasics/LearningJavaBasics.java
@@ -0,0 +1,32 @@
+/*
+ * Name: David Hjelm
+ * Period: 2
+ * Program: Learning Java Basics
+ */
+package learningjavabasics;
+
+/**
+ *
+ * @author 19411
+ */
+public class LearningJavaBasics {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        int x = 10;
+        System.out.println ("the value of x is + x");
+        
+        String name = "John Jacob Jinglehiemer Schmidt"; 
+        System.out.println ("His nam is : " + name);
+        int num1 = 2;
+        int num2 = 2;
+        System.out.println (num1 + num2 + " is my favorite number");
+        System.out.println ("My favorite number is " + num1 + num2);
+        
+        
+    }
+    
+}
